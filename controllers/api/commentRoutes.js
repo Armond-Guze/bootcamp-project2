4f4fc78d02538e498f5856d70ca174efc732595d
Mona Lisa <147671781+ajcasablanca@users.noreply.github.com>
2024-02-07 21:11:01 -0500

    updated comments and seeds

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,21 +1,31 @@
 const router = require('express').Router();
-const { User } = require('../../../RUT-VIRT-FSF-PT-10-2023-U-LOLC/14-MVC/01-Activities/28-Stu_Mini-Project/Main/models');
-const { Comment } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
-      const newPost = await Comment.create({
+      const newComment = await Comment.create({
         ...req.body,
         user_id: req.session.user_id,
       });
   
-      res.status(200).json(newPost);
+      res.status(200).json(newComment);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
     }
   });
 
-  router.get('/', withAuth, async(req, res) => {
+  router.get('/:id', withAuth, async(req, res) => {
     try {
-       
+        const comments = await Comment.findAll({
+           where:{post_id: req.params.id}
+            
+          });
+      
+          res.status(200).json(newPost);
+    } catch (err) {
+        res.status(500).json(err);
+      }
+})
+
+module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,35 +1,29 @@
 $(document).ready(function() {
     // Event listener for the submit button click event
-    $("#submit").on("click", function(event) {
+    $("#submit").on("click", async function(event) {
         event.preventDefault();
         
         // Collect comment data from input fields
-        var username = $("#usernameInput").val();
         var commentText = $("#commentBody").val();
-        var createdAt = new Date(); // Current date/time
         
         // Construct the comment object
         var newComment = {
-            user: {
-                username: username
-            },
+            
             comment_body: commentText,
-            createdAt: createdAt
         };
-
-        // Call a function to post the comment onto the page
-        postComment(newComment);
+        const response = await fetch('/api/comment', {
+                method: 'POST',
+                body: JSON.stringify(newComment),
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                   
+        }) 
+        if (response.ok) {
+            document.location.reload();
+          } else {
+            alert('Failed to create post');
+          }
     });
 });
 
-// Function to post the comment onto the page
-function postComment(comment) {
-    // Compile the comment card template using Handlebars
-    var template = Handlebars.compile($("#comment-card").html());
-    
-    // Pass the comment data to the compiled template to create the HTML
-    var html = template(comment);
-    
-    // Append the HTML to the comments section on the page
-    $(".comments").append(html);
-}
